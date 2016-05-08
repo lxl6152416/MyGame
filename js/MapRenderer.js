87b853714c889e395564a06e6bd79bb76a579565
Gibber <375352028@qq.com>
2016-05-09 07:36:20 +0800

    v0.11

--- js/MapRenderer.js
+++ js/MapRenderer.js
@@ -2,45 +2,74 @@ function MapRenderer(mapData, image, tileSize, viewportWidth, viewportHeight) {
     this._mapData = mapData;
     this._image = image;
     this._tileSize = tileSize;
+    this._viewportWidth = viewportWidth;
+    this._viewportHeight = viewportHeight;
 
     // Coordinates of the map
     this._x = 0;
     this._y = 0;
+
+    // Offscreen buffer
+    this._offCanvas = document.createElement("canvas");
+    this._offContext = this._offCanvas.getContext("2d");
+    this._offBounds = {x: 0, y:0, w: 0, h: 0};
+    this._offDirty = true;
+    this._resetOffScreenCanvas();
+
     // The number of tiles in one row of the image
     this._tilesPerRow = image.width/tileSize;
-
-    //屏幕外缓冲画布
-    this._offCanvas = document.createElement("canvas");
-    this._offContext = this._offCanvas.getContext('2d');
-    this._offDirty = true;
-    this.setViewportSize(viewportWidth , viewportHeight);
 }
 
 _p = MapRenderer.prototype;
 
-_p.draw = function (ctx) {
-    if(this._offDirty){
+/* Draws the whole map */
+_p.draw = function(ctx) {
+    if (this._offDirty) {
         this._redrawOffscreen();
     }
-    ctx.drawImage(this._offCanvas,0,0);
-}
-/* Draws the whole map */
-_p._redrawOffscreen = function() {
 
+    var offCanvasX = -Math.floor(this._x) - this._offBounds.x*this._tileSize;
+    var offCanvasY = -Math.floor(this._y) - this._offBounds.y*this._tileSize;
+    var offCanvasW = Math.min(this._offCanvas.width - offCanvasX, this._viewportWidth);
+    var offCanvasH = Math.min(this._offCanvas.height - offCanvasY, this._viewportHeight);
+
+    ctx.drawImage(this._offCanvas, offCanvasX, offCanvasY, offCanvasW, offCanvasH,
+        0, 0, offCanvasW, offCanvasH);
+};
+
+
+_p.move = function(deltaX, deltaY) {
+    this._x += deltaX;
+    this._y += deltaY;
+    this._updateOffscreenBounds();
+};
+
+_p.setViewportSize = function(width, height) {
+    this._viewportWidth = width;
+    this._viewportHeight = height;
+    this._resetOffScreenCanvas();
+};
+
+_p._resetOffScreenCanvas = function() {
+    this._updateOffscreenBounds();
+    this._offCanvas.width = this._offBounds.w*this._tileSize;
+    this._offCanvas.height = this._offBounds.h*this._tileSize;
+    this._offDirty = true;
+};
+
+/*_p._redrawOffscreen = function() {
     var ctx = this._offContext;
     ctx.clearRect(0, 0, this._viewportWidth, this._viewportHeight);
-    //上下左右可见瓦片的坐标.
-    var startX = Math.floor(-this._x / this._tileSize);
-    startX = Math.max(startX, 0);
-    var endX = Math.floor((this._viewportWidth - this._x)/this._tileSize);
-    console.log(this._mapData[0].length - 1+","+endX);
-    endX = Math.min(endX, this._mapData[0].length - 1);
 
+    var startX = Math.max(Math.floor(-this._x / this._tileSize), 0);
+    var endX = Math.min(
+        Math.floor((this._viewportWidth - this._x) / this._tileSize),
+        this._mapData[0].length - 1);
 
-    var startY = Math.floor(-this._y / this._tileSize);
-    startY = Math.max(startY , 0);
-    var endY = Math.floor((this._viewportHeight - this._y) / this._tileSize);
-    endY = Math.min(endY, this._mapData.length - 1);
+    var startY = Math.max(Math.floor(-this._y / this._tileSize), 0);
+    var endY = Math.min(
+        Math.floor((this._viewportHeight - this._y) / this._tileSize),
+        this._mapData.length - 1);
 
     for (var cellY = startY; cellY <= endY; cellY++) {
         for (var cellX = startX; cellX <= endX; cellX++) {
@@ -49,6 +78,25 @@ _p._redrawOffscreen = function() {
         }
     }
 
+    this._offDirty = false;
+};*/
+
+_p._redrawOffscreen = function() {
+    var ctx = this._offContext;
+    ctx.clearRect(0, 0, this._offCanvas.width, this._offCanvas.height);
+
+    var startX = Math.max(this._offBounds.x, 0);
+    var endX = Math.min(startX + this._offBounds.w - 1, this._mapData[0].length - 1);
+
+    var startY = Math.max(this._offBounds.y, 0);
+    var endY = Math.min(startY + this._offBounds.h - 1, this._mapData.length - 1);
+
+    for (var cellY = startY; cellY <= endY; cellY++) {
+        for (var cellX = startX; cellX <= endX; cellX++) {
+            var tileId = this._mapData[cellY][cellX];
+            this._drawTileAt(ctx, tileId, cellX, cellY);
+        }
+    }
     this._offDirty = false;
 };
 
@@ -62,27 +110,27 @@ _p._drawTileAt = function(ctx, tileId, cellX, cellY) {
     // size of the tile
     var size = this._tileSize;
 
-    // position of the tile on the screen
-    var destX = this._x + cellX*size;
-    var destY = this._y + cellY*size;
+    // position of the tile on the offscreen buffer
+    var destX = (cellX - this._offBounds.x)*size;
+    var destY = (cellY - this._offBounds.y)*size;
 
     ctx.drawImage(this._image, srcX, srcY, size, size, destX, destY, size, size);
 };
 
-_p.move = function(deltaX, deltaY) {
-    Drawable.prototype.move.call(this, deltaX, deltaY);
-    this._offDirty = true;
-};
+_p._updateOffscreenBounds = function() {
+    var newBounds = {
+        x: Math.floor(-this._x / this._tileSize),
+        y: Math.floor(-this._y / this._tileSize),
+        w: Math.ceil(this._viewportWidth/this._tileSize) + 1,
+        h: Math.ceil(this._viewportHeight/this._tileSize) + 1
+    };
 
-_p.setViewportSize = function(Width, height){
-    this._viewportWidth = Width;
-    this._viewportHeight = height;
-    this._resetOffScreenCanvas();
+    var oldBounds = this._offBounds;
+    if (!(oldBounds.x == newBounds.x
+        && oldBounds.y == newBounds.y
+        && oldBounds.w == newBounds.w
+        && oldBounds.h == newBounds.h)) {
+        this._offBounds = newBounds;
+        this._offDirty = true;
+    }
 };
-
-_p._resetOffScreenCanvas = function(){
-    this._offCanvas.width = this._viewportWidth;
-    this._offCanvas.heigh = this._viewportHeight;
-    this,_offDirty = true;
-};
-
